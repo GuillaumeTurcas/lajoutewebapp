10ef93196ee8d65904a9463468494fd47552d42a
GuillaumeTurcas <62696687+GuillaumeTurcas@users.noreply.github.com>
2021-04-07 23:37:48 +0200

    Update config [Git Version].py

--- private/config/config [Git Version].py
+++ private/config/config [Git Version].py
@@ -5,8 +5,15 @@ user = 'user'
 passwd = 'passwd'
 db = 'db'
 secret_key = 'secret_key'
-salt = binascii.unhexlify('salt')
-iterations = 1
-dksize = 1
+
+salt = binascii.unhexlify('salt') #change the salt
+
+hf_name = 'sha512'
+iterations = 1 #change the number
+dksize = 1 #change the number
+
+hf_name_pepper = 'sha512'
+iterations_pepper = 1 #change the number
+dksize_pepper = 1 #change the number
 
 defaultpass = 'mdp'
